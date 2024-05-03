7625ae8996db3ec22968b665f5baabe7ebd59d41
Umang Rathi <umangrathi110@gmail.com>
2024-05-03 18:52:22 +0530

    updatedall file

---
+++ setup.md
@@ -0,0 +1,21 @@
+MCQ Generator using OpenAI and Langchain 
+
+steps -
+step 1 -> setting up the development environment 
+step 2 -> few experiment in jupyter lab 
+step 3 -> converted into modular coding 
+step 4 -> creating web API using streamlit 
+Step 5 -> Deploy application on cloud platform (AWS)
+
+
+
+
+<!-- Why we created the __init__.py file  -->
+in the src folder there is the folder mcq_generator inside it there are several files regarding the project and to consider it as a package there is a standard to add this file.
+
+<!-- run 
+pip install -r requirement.txt 
+this will create a folder containing all the information regarding your local package-->
+
+
+=> inside the experiments folder i have created the ipynb file (jupyter notebooks)
---
+++ setup.py
@@ -0,0 +1,25 @@
+# for installing the local packages in the virtual environemnt
+from setuptools import find_packages, setup
+
+setup(
+    name='mcq_generator',
+    version='0.0.1',
+    author='Umang Rathi',
+    author_email='umangrathi110@gmail.com',
+    install_requires=['openai', 'langchain', 'streamlit', 'python-dotenv','PyPdf2'],
+    packages=find_packages()   
+    # this find_packages() is responsible for finding the local packages from you local directiory (whenever it foudnd the __init__.py it consider this as a package)
+)
+
+
+# if want to install the package 
+# pip install package_name
+
+# command to install the requirements (installing package from requirement)
+# pip install -r requirement.txt 
+
+# for installing local package into your current virtual environment 
+# python setup.py install 
+
+# another method mention -e . in the requirement.txt automatically it search out the packages in your folder and execute the setup.py in the backend
+
